5d357be91ecdd33a94a2f2c4c878ded3a5e70082
Taylor Sutton <tsutton125@gmail.com>
2022-12-24 13:33:07 -0800

    Day 15 part 2

--- src/day15.rs
+++ src/day15.rs
@@ -107,25 +107,8 @@ pub fn part1() {
 
     let sensor_beacon_pairs: Vec<_> = input.lines().map(parse_line).collect();
 
-    let mut uncombined_intervals: Vec<_> = sensor_beacon_pairs
-        .iter()
-        .map(|(sensor, beacon)| no_beacon_spaces_with_y_from_pair(sensor, beacon, row))
-        .filter(|t| t != &(0, 0))
-        .collect();
+    let combined_intervals = intervals_of_no_beacon_in_y(&sensor_beacon_pairs, row);
 
-    uncombined_intervals.sort_by_key(|interval| interval.0);
-    let mut combined_intervals = vec![uncombined_intervals[0]];
-    for interval in uncombined_intervals[1..].iter() {
-        // check if interval overlaps with last interval in combined
-        // if so, union them, if not append
-        let current_len = combined_intervals.len();
-        let previous = combined_intervals[current_len - 1];
-        if interval.0 <= previous.1 {
-            combined_intervals[current_len - 1].1 = interval.1.max(previous.1);
-        } else {
-            combined_intervals.push((interval.0, interval.1));
-        }
-    }
     let total_length: i64 = combined_intervals.iter().map(|l| l.1 - l.0).sum();
     let beacons_in_row: HashSet<_> = sensor_beacon_pairs
         .iter()
@@ -142,4 +125,77 @@ pub fn part1() {
     );
 }
 
-pub fn part2() {}
+fn intervals_of_no_beacon_in_y(pairs: &[(Sensor, Beacon)], y: i64) -> Vec<(i64, i64)> {
+    let mut uncombined_intervals: Vec<_> = pairs
+        .iter()
+        .map(|(sensor, beacon)| no_beacon_spaces_with_y_from_pair(sensor, beacon, y))
+        .filter(|t| t != &(0, 0))
+        .collect();
+
+    uncombined_intervals.sort_by_key(|interval| interval.0);
+    let mut combined_intervals = vec![uncombined_intervals[0]];
+    for interval in uncombined_intervals[1..].iter() {
+        // check if interval overlaps with last interval in combined
+        // if so, union them, if not append
+        let current_len = combined_intervals.len();
+        let previous = combined_intervals[current_len - 1];
+        if interval.0 <= previous.1 {
+            combined_intervals[current_len - 1].1 = interval.1.max(previous.1);
+        } else {
+            combined_intervals.push((interval.0, interval.1));
+        }
+    }
+    combined_intervals
+}
+
+// given a slice of disjoint intervals (half-open ) sorted by starting point, and another interval,
+// return None if the given single interval is completely contained in the list of intevals,
+// and Some(x) if the given interval is not contained, where x is the smallest value in the given interval
+// not contained in the list
+fn sorted_intervals_point_between(intervals: &[(i64, i64)], min: i64, max: i64) -> Option<i64> {
+    // find the first interval whose start is >= min
+    match intervals.binary_search_by_key(&min, |interval| interval.0) {
+        Ok(idx) => {
+            // intervals[idx].0 == min
+            if intervals[idx].1 >= max {
+                None
+            } else {
+                Some(intervals[idx].1)
+            }
+        }
+        Err(idx) => {
+            // either intervals[idx].0 > min and intervals[idx-1].0 < min, or
+            // idx could be 0 or intervals.len()
+            if idx == 0 {
+                Some(min)
+            } else if intervals[idx - 1].1 >= max {
+                None
+            } else {
+                Some(intervals[idx - 1].1)
+            }
+        }
+    }
+}
+
+pub fn part2() {
+    // first idea: brute force, iterate through all 4000000^2 places.
+    // slightly faster idea using the building block in part 1: iterate only through x-values
+    // and for each value, do the part-1 equivalent. Let's try that.
+    // let input = EXAMPLE;
+    // let min_coord = 0;
+    // let max_coord = 20;
+
+    let input = read_input();
+    let min_coord = 0;
+    let max_coord = 4_000_000;
+
+    let sensor_beacon_pairs: Vec<_> = input.lines().map(parse_line).collect();
+
+    for y in min_coord..max_coord {
+        let intervals = intervals_of_no_beacon_in_y(&sensor_beacon_pairs, y);
+        if let Some(x) = sorted_intervals_point_between(&intervals, min_coord, max_coord) {
+            println!("({x}, {y}) => {}", x * 4_000_000 + y);
+            return;
+        }
+    }
+}
